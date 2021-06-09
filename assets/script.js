ff70aeb235309e37c6ee624be4f901c4068d48fb
St.Onge <stonge.ms@gmail.com>
2021-06-08 17:44:54 -0700

    cleaned up the css and background images

--- assets/script.js
+++ assets/script.js
@@ -218,7 +218,7 @@ const questions = [
     answers: [
       { text: "//", correct: false },
       { text: "<!--", correct: false },
-      { text: "/*/", correct: true },
+      { text: "/**/", correct: true },
       { text: "#", correct: false },
     ],
   },
